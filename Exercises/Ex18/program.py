4a74c32a87fb08c95070853d05faa75cc9abfb4d
yukienomiya <yn.yuki98@gmail.com>
2020-04-14 17:24:35 +0200

    Add exercise 18

---
+++ Exercises/Ex18/program.py
@@ -0,0 +1,27 @@
+def es18(d1, d2):
+  '''
+  Es 9: 3 punti
+  Si definisca la  funzione es18(d1,d2) che, 
+  - riceve due dizionari aventi per chiavi degli interi e per attributo insiemi di interi.
+  - restituisce un dizionario.
+  il dizionaro deve contenere come chiavi le chiavi che sono in comune ad entrambi i dizionari e come 
+  attributo una tupla di due elementi, il primo elemento e' 
+  l'insieme intersezione degli attributi della chiave nei due dizionari mentre 
+  il secondo e' l'unione degli attributi della chiave nei due dizionari.
+  Ad ESEMPIO se
+  d1={1: {1,2,3}, 2:{1,2,3}, 5:{1} } e 
+  d2={1: {3,4,5}, 3:{1,2,3}, 5:{3}, 8: {6} }
+  allora la funzione  restituisce il dizionario 
+  {1: ({3}, {1, 2, 3, 4, 5}), 5: (set(), {1, 3})}
+  '''
+  
+  res = dict()
+  d1K = set(d1.keys())
+  d2K = set(d2.keys())
+  kSet = d1K.intersection(d2K)
+
+  for k in kSet:
+    intSet = (d1[k]).intersection(d2[k])
+    unSet = (d1[k]).union(d2[k])
+    res[k] = (intSet, unSet)
+  return res
